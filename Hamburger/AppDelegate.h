64877555ad1db9df0f311a9e962b4a9a3da1c19c
Jon Burgermeister <jdburgie@gmail.com>
2014-12-02 11:37:08 -0700

    Initial commit
    Hamburger

---
+++ Hamburger/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hamburger
+//
+//  Created by jdburgie on 12/2/14.
+//  Copyright (c) 2014 ThreeOakWood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hamburger/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Hamburger
+//
+//  Created by jdburgie on 12/2/14.
+//  Copyright (c) 2014 ThreeOakWood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
